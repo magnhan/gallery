dfc4fc425a7bef711b30a2ca4c58aa836e1849f2
aleksgro <aleksgro@stud.ntnu.no>
2020-09-09 12:33:27 +0200

    Initial commit update

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -5,25 +5,32 @@ import './App.css';
 class App extends Component {
   constructor(){
     super();
-      
-    this.state = {
-      string: 'Hello this is our gallery'
+
+    this.state ={
+      string: "Hello this is our gallery"
     }
   }
+
   render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={()=> this.setState({string: 'Text is changed'})}>Change text</button>
-        </header>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {this.state.string}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
   }
-  
 }
 
 export default App;
