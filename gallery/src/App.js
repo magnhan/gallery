f110776182911864dfeef4cb79482f9e36965af6
aleksgro <aleksgro@stud.ntnu.no>
2020-09-11 12:23:15 +0200

    test

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
-
     this.state ={
-      string: "Hello this is our gallery"
+      string: 'Test'
     }
   }
 
@@ -15,18 +13,9 @@ class App extends Component {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
