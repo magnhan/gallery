176dc2c1f8dbff8c60c8d61be40da0e9c3b718b8
aleksgro <aleksgro@stud.ntnu.no>
2020-09-14 10:05:16 +0200

    update_typescipt

---
+++ gallery/src/components/displayArt.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function displayArt(){
+    return (
+        <div className='displayArt'>
+            <svg className='SVG1' height='300px' width='300px'></svg>
+            <svg className='SVG1' height='300px' width='300px'></svg>
+        </div>
+    )
+}
