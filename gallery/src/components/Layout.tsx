b04ae554346b1f55441dd64c92c3746419eadbd0
aleksgro <aleksgro@stud.ntnu.no>
2020-09-15 10:55:09 +0200

    udated grid

--- gallery/src/components/Layout.tsx
+++ gallery/src/components/Layout.tsx
@@ -7,22 +7,22 @@ export default function Layout() {
             <div className="Menu">Menu</div>
             <div className="DisplayArt">
                 <div className="SVG 1">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
                 <div className="SVG 2">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
                 <div className="SVG 3">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
                 <div className="SVG 4">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
                 <div className="SVG 5">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
                 <div className="SVG 6">
-                    <svg height='300px' width='300px'></svg>
+                    <svg height='250px' width='300px'></svg>
                 </div>
             </div>  
             <div className="Footer">Footer</div>
