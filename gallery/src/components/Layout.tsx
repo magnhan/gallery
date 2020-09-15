916dfe0320ca10aed53692f211b84fabc21d1143
Anchana Balasingham <anchanab@stud.ntnu.nu>
2020-09-15 13:54:49 +0200

    Made small changes to the svg-div

--- gallery/src/components/Layout.tsx
+++ gallery/src/components/Layout.tsx
@@ -1,32 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Layout() {
-    return (
-        <div className="grid-container">
-            <div className="Header">Header</div>
-            <div className="Menu">Menu</div>
-            <div className="DisplayArt">
-                <div className="SVG 1">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-                <div className="SVG 2">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-                <div className="SVG 3">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-                <div className="SVG 4">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-                <div className="SVG 5">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-                <div className="SVG 6">
-                    <svg height='250px' width='300px'></svg>
-                </div>
-            </div>  
-            <div className="Footer">Footer</div>
+  return (
+    <div className="grid-container">
+      <div className="Header">Header</div>
+      <div className="Menu">Menu</div>
+      <div className="DisplayArt">
+        <div className="SVG 1">
+          <svg height="250px" width="280px"></svg>
         </div>
-    )
-    
+        <div className="SVG 2">
+          <svg height="250px" width="280px"></svg>
+        </div>
+        <div className="SVG 3">
+          <svg height="250px" width="280px"></svg>
+        </div>
+        <div className="SVG 4">
+          <svg height="250px" width="280px"></svg>
+        </div>
+        <div className="SVG 5">
+          <svg height="250px" width="280px"></svg>
+        </div>
+        <div className="SVG 6">
+          <svg height="250px" width="280px"></svg>
+        </div>
+      </div>
+      <div className="Footer">Footer</div>
+    </div>
+  );
 }
