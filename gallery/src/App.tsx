c1f65d27766cfeedf2f6e9549dbbdf22c2ac2c52
Anchana Balasingham <anchanab@stud.ntnu.nu>
2020-09-17 22:33:17 +0200

    Updated react-dom and added component to App.tsx

--- gallery/src/App.tsx
+++ gallery/src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Transition from "./components/Transition";
 
-function App() {
-  return (
-    <div className="App">
-        <p>test</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+    <Switch>
+      <Route exact path="components/Transition" component={Transition}></Route>
+    </Switch>;
+  }
 }
 
 export default App;
--- gallery/src/index.tsx
+++ gallery/src/index.tsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Layout from './components/Layout';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Layout from "./components/Layout";
 
-const grid = (
-  <Layout/>
-)
+const grid = <Layout />;
 
-ReactDOM.render(
-  grid,
-  document.getElementById('root')
-);
+ReactDOM.render(grid, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
