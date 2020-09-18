8030942666038710d109448f77d9197a82f9b66e
Anchana Balasingham <anchanab@stud.ntnu.nu>
2020-09-18 09:50:53 +0200

    Changed filename

--- gallery/src/components/Layout.tsx
+++ gallery/src/components/Layout.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import DisplayArt from './DisplayArt';
-
+import DisplayArt from "./DisplayArt";
 
 export default function Layout() {
   return (
     <div className="grid-container">
       <div className="Header">Header</div>
       <div className="Menu">Menu</div>
-      <DisplayArt/>
+      <DisplayArt />
       <div className="Footer">Footer</div>
     </div>
   );
