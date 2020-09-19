a23fb84204eab2819ccfbb8345bce588a8b47332
Anchana Balasingham <anchanab@stud.ntnu.nu>
2020-09-19 14:43:09 +0200

    Added some animation to svg1
    #6

--- gallery/src/App.tsx
+++ gallery/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "./index.css";
+import "./svgPaintings.css";
 import DisplayArt from "./components/DisplayArt";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
