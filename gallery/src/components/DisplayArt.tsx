1ddd00bf906c25fb8c7704b4b94cbcf4e3933f9b
aleksgro <aleksgro@stud.ntnu.no>
2020-09-23 11:47:45 +0200

    Moved Poetry.tsx into DisplayArt.tsx
    #4

--- gallery/src/components/DisplayArt.tsx
+++ gallery/src/components/DisplayArt.tsx
@@ -1,38 +1,72 @@
-import React from "react";
-import Poetry from './Poetry'
+import React, { Component } from "react";
 
-export default function DisplayArt() {
-  return (
-    <div className="DisplayArt">
-      <div className="SVG 1">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
+class DisplayArt extends Component<{}, {poetryDB: Readonly<any>} > {
+    constructor(props: any){
+      super(props);
+      console.log(this.props);
+  
+      this.state = {
+        poetryDB: []
+      };
+    }
+    
+    //Fetches poetry from the online database
+    componentDidMount() {
+      fetch('https://poetrydb.org/author,title/Shakespeare;Sonnet')
+        .then(response => response.json())
+        .then(users => this.setState({poetryDB: users}));
+    }
 
-      <div className="SVG 2">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
+    //Generates one random poetry title
+    generatePoetry(){
+      return(
+        <div>
+          {
+            this.state.poetryDB.map((poem: { title: string | number | null | undefined; lines: React.ReactNode; }) => (
+            <p key={poem.title} style={{color:"white", fontSize:"x-small"}}>
+              {poem.title}
+            </p>
+            ))[Math.floor(Math.random() * (this.state.poetryDB.length - 1))] 
+          }
+        </div>
+      )
+    }
 
-      <div className="SVG 3">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
+    render(){
+        return(
+        <div className="DisplayArt">
+          <div className="SVG 1">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
 
-      <div className="SVG 4">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
+          <div className="SVG 2">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
 
-      <div className="SVG 5">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
+          <div className="SVG 3">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
 
-      <div className="SVG 6">
-        <svg className="SVG_picture"></svg>
-        <Poetry/>
-      </div>
-    </div>
-  );
+          <div className="SVG 4">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
+
+          <div className="SVG 5">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
+
+          <div className="SVG 6">
+            <svg className="SVG_picture"></svg>
+            {this.generatePoetry()}
+          </div>
+        </div>
+        )
+    }
 }
+
+export default DisplayArt; 
