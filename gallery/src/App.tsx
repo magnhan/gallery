78baa3a8cc590b81ebbc248b7a158895715aca5e
aleksgro <aleksgro@stud.ntnu.no>
2020-09-23 12:28:27 +0200

    Added soud, not finished
    #5

--- gallery/src/App.tsx
+++ gallery/src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './sang.mp3';
 
 function App() {
+  function sound(){
+    console.log('hei');
+    return (
+      <audio controls>
+        <source src={require("./sang.mp3")} type="audio/mpeg"/>
+      </audio>
+    )
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <audio controls>
+          <source src={require("./sang.mp3")} type="audio/mpeg"/>
+        </audio>
+        <button onClick={sound}> click me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,3 +38,4 @@ function App() {
 }
 
 export default App;
+
