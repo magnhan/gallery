ca21133af02f1babbe1f6d2a945ba55e8fbc20c8
magnhan <45832668+magnhan@users.noreply.github.com>
2020-09-23 20:48:54 +0200

    Merged App.tsx and index.tsx back into svg
    I fucked up

--- gallery/src/App.tsx
+++ gallery/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./index.css";
+import DisplayArt from "./components/DisplayArt";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <Header />
+      <Menu />
+      <DisplayArt />
+      <Footer />|
     </div>
   );
 }
-
-export default App;
