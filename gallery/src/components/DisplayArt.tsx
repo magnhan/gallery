daab2e7b9495c1f8388bf79d1a740ad21d567dcc
magnhan <45832668+magnhan@users.noreply.github.com>
2020-09-25 11:40:00 +0200

    Fetches poetry once and passes as props
    #4

--- gallery/src/components/DisplayArt.tsx
+++ gallery/src/components/DisplayArt.tsx
@@ -7,14 +7,13 @@ import SVG5 from "./paintings/SVG5/SVG5";
 import SVG6 from "./paintings/SVG6/SVG6";
 import "./display-art.css";
 
-class DisplayArt extends Component<{}, {poetryDB: Readonly<any>, poems: Map<string | number | null, React.ReactNode>} > {
+class DisplayArt extends Component<{}, {poetryDB: Readonly<any>} > {
     constructor(props: any){
       super(props);
       console.log(this.props);
       
       this.state = {
-        poetryDB: [],
-        poems: new Map()
+        poetryDB: []
       };
     }
     
@@ -23,14 +22,14 @@ class DisplayArt extends Component<{}, {poetryDB: Readonly<any>, poems: Map<stri
       fetch('https://poetrydb.org/author,title/Shakespeare;Sonnet')
         .then(response => response.json())
         .then(users => this.setState({poetryDB: users}));
-      poems = this.state.poetryDB.map((poem: { title: string | number | null | undefined; lines: React.ReactNode; }) => (
-        poem.title))
     }
 
     //Generates one random poetry title
     generatePoetry(){
+      //Extracts only the titles and picks one at random from the array
       return(
-        poems[Math.floor(Math.random() * (this.state.poetryDB.length - 1))]
+        this.state.poetryDB.map((poem: { title: string | number | null | undefined; lines: React.ReactNode; }) => (
+          poem.title))[Math.floor(Math.random() * (this.state.poetryDB.length - 1))]
         )
     }
     
