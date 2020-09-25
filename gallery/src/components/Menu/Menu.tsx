7b59918857a2122c2d1e396eaaa4e88230b07a51
aleksgro <aleksgro@stud.ntnu.no>
2020-09-25 15:03:04 +0200

    added fonts
    #2

--- gallery/src/components/Menu/Menu.tsx
+++ gallery/src/components/Menu/Menu.tsx
@@ -9,13 +9,13 @@ export default function Menu() {
         <li>
           <NavLink to="/">Gallery</NavLink>
         </li>
-        <li>
+        <li id ='Tropical'>
           <NavLink to="/Theme1">Tropical</NavLink>
         </li>
-        <li>
+        <li id ='Disco'>
           <NavLink to="/Theme2">Disco</NavLink>
         </li>
-        <li>
+        <li id ='Royal'>
           <NavLink to="/Theme3">Royal</NavLink>
         </li>
         <li>
