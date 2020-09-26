d6c55ef0a3eb3ea62df9aff38bb44e175e497fa2
magnhan <45832668+magnhan@users.noreply.github.com>
2020-09-26 16:02:08 +0200

    Added all installations
    #3

--- gallery/src/components/DisplayArt.tsx
+++ gallery/src/components/DisplayArt.tsx
@@ -6,7 +6,12 @@ import Installation from "./paintings/Installation";
 export default function DisplayArt() {
   return (
     <div className="DisplayArt">
-      <Installation image={1} poem={"yeet"}/>
+      <Installation image={1} poem={"yeet"} />
+      <Installation image={2} poem={"yeet"} />
+      <Installation image={3} poem={"yeet"} />
+      <Installation image={4} poem={"yeet"} />
+      <Installation image={5} poem={"yeet"} />
+      <Installation image={6} poem={"yeet"} />
     </div>
   );
 }
