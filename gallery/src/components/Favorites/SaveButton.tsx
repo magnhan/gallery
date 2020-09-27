26cc83e52cd3dad955efd64b5246bc5123686ff0
aleksgro <aleksgro@stud.ntnu.no>
2020-09-27 21:06:11 +0200

    Added save button star
    #14

---
+++ gallery/src/components/Favorites/SaveButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SaveButton() {
+  return (
+    <div className= "SaveButton">
+        <svg width="70px" height="70px">
+          <path
+            d="M 46.296296,51.906272 L 31.916351,42.474649 L 17.502712,51.8547 L
+                22.029072,35.264028 L 8.654054,24.454438 L 25.831443,23.632463 L
+                31.978866,7.5717174 L 38.068716,23.65438 L 55.243051,24.537884 L
+                41.829396,35.299492 L 46.296296,51.906272 z "
+          />
+        </svg>
+    </div>
+  );
+}
+
+
+
--- gallery/src/components/paintings/Installation.tsx
+++ gallery/src/components/paintings/Installation.tsx
@@ -7,6 +7,7 @@ import SVG4 from "./SVG4/SVG4";
 import SVG5 from "./SVG5/SVG5";
 import SVG6 from "./SVG6/SVG6";
 import "./svg.css";
+import SaveButton from "../Favorites/SaveButton";
 
 export default function Installation(props: any) {
     switch(props.image) {
@@ -14,42 +15,60 @@ export default function Installation(props: any) {
             return (
                 <div className="SVG SVG1">
                     <SVG1/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
             );
         case 2:
             return (
                 <div className="SVG SVG2">
                     <SVG2/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
             );
         case 3:
             return (
                 <div className="SVG SVG3">
                     <SVG3/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
             );
         case 4:
             return (
                 <div className="SVG SVG4">
                     <SVG4/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
             );
         case 5:
             return (
                 <div className="SVG SVG5">
                     <SVG5/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
                 );
         case 6:
             return (
                 <div className="SVG SVG6">
                     <SVG6/>
+                    <div className="caption">
                     <p className="text">{props.poem}</p>
+                    <SaveButton/>
+                    </div>
                 </div>
             );
         default:
