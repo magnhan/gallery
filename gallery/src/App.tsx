40c4dc4059c926133c97503f0d770ac4e0041eb5
Anchana Balasingham <anchanab@stud.ntnu.nu>
2020-09-28 15:39:48 +0200

    Removed unused imports and cleaned up some files
    #10

--- gallery/src/App.tsx
+++ gallery/src/App.tsx
@@ -6,36 +6,38 @@ import Header from "./components/Header";
 import Menu from "./components/Menu/Menu";
 import Footer from "./components/Footer";
 import FavoritesButton from "./components/Favorites/FavoritesButton";
-import ThemeContex, {themes} from './components/ThemeContext';
+import ThemeContex, { themes } from "./components/ThemeContext";
 import { HashRouter } from "react-router-dom";
-
 
 export default function App() {
   const [theme, setTheme] = useState(themes.standard);
-  const tropicalThemeToggle = () => (theme !== themes.tropical?
-    setTheme(themes.tropical):setTheme(themes.standard)
-  );
-  const discoThemeToggle = () => (theme !== themes.disco?
-    setTheme(themes.disco):setTheme(themes.standard)
-  );
-  const royalThemeToggle = () => (theme !== themes.royal?
-    setTheme(themes.royal):setTheme(themes.standard)
-  );
+  const tropicalThemeToggle = () =>
+    theme !== themes.tropical
+      ? setTheme(themes.tropical)
+      : setTheme(themes.standard);
+  const discoThemeToggle = () =>
+    theme !== themes.disco ? setTheme(themes.disco) : setTheme(themes.standard);
+  const royalThemeToggle = () =>
+    theme !== themes.royal ? setTheme(themes.royal) : setTheme(themes.standard);
 
   return (
-    <ThemeContex.Provider value= {{theme, tropicalThemeToggle, discoThemeToggle, royalThemeToggle}}>
-    <HashRouter>
-      <div className="grid-container" style={{backgroundColor: theme.background}}>
-        <FavoritesButton />
-        <Header />
-        <Menu/>
-        <div className="contentDiplayed">
-        <DisplayArt/>
+    <ThemeContex.Provider
+      value={{ theme, tropicalThemeToggle, discoThemeToggle, royalThemeToggle }}
+    >
+      <HashRouter>
+        <div
+          className="grid-container"
+          style={{ backgroundColor: theme.background }}
+        >
+          <FavoritesButton />
+          <Header />
+          <Menu />
+          <div className="contentDiplayed">
+            <DisplayArt />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
       </HashRouter>
     </ThemeContex.Provider>
-    
   );
 }
