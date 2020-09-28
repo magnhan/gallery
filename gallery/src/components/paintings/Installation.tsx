d0072708b5567fd70a3134870bd2cb75402c7c25
magnhan <45832668+magnhan@users.noreply.github.com>
2020-09-28 17:30:53 +0200

    Removed redundant class calls

--- gallery/src/components/paintings/Installation.tsx
+++ gallery/src/components/paintings/Installation.tsx
@@ -19,42 +19,42 @@ export default function Installation(props: installationProps) {
   switch (props.image) {
     case 1:
       return (
-        <div className="SVG SVG1" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG1 />
           <Poem poems= {props.poem}/>
         </div>
       );
     case 2:
       return (
-        <div className="SVG SVG2" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG2 />
           <Poem poems= {props.poem}/>
         </div>
       );
     case 3:
       return (
-        <div className="SVG SVG3" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG3 />
           <Poem poems= {props.poem}/>
         </div>
       );
     case 4:
       return (
-        <div className="SVG SVG4" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG4 />
           <Poem poems= {props.poem}/>
         </div>
       );
     case 5:
       return (
-        <div className="SVG SVG5" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG5 />
           <Poem poems= {props.poem}/>
         </div>
       );
     case 6:
       return (
-        <div className="SVG SVG6" style={{ backgroundColor: theme.photo }}>
+        <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG6 />
           <Poem poems= {props.poem}/>
         </div>
