88886454e2a412e60bf0a53699f7b7035ff4b950
magnhan <45832668+magnhan@users.noreply.github.com>
2020-09-28 19:06:12 +0200

    Reverted commits and fixed Installation.tsx

--- gallery/src/components/paintings/Installation.tsx
+++ gallery/src/components/paintings/Installation.tsx
@@ -8,6 +8,8 @@ import SVG6 from "./SVG6/SVG6";
 import "./svg.css";
 import ThemeContext from "../ThemeContext";
 import Poem from "../Poem";
+import SaveButton from "../Favorites/SaveButton";
+import "./../Favorites/Favorites.css"
 
 interface installationProps {
   image: number;
@@ -21,42 +23,60 @@ export default function Installation(props: installationProps) {
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG1 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     case 2:
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG2 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     case 3:
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG3 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     case 4:
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG4 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     case 5:
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG5 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     case 6:
       return (
         <div className="SVG" style={{ backgroundColor: theme.photo }}>
           <SVG6 />
+          <div className="caption">
           <Poem poems={props.poem} />
+          <SaveButton image={props.image}/>
+          </div>
         </div>
       );
     default:
